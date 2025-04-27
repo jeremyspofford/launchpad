591c6967725129754c62a0cf5165b7b2886252c1
Jeremy Spofford <23528024+jeremyspofford@users.noreply.github.com>
2025-04-27 11:52:24 -0400

    add generate ssh key function to init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -1,68 +1,3 @@
-# #!/bin/bash
-
-# repo_name="dotfiles"
-
-# command_exists() {
-#     command -v "$1" &> /dev/null
-# }
-
-# # Generate SSH key
-# generate_ssh_key() {
-#     mkdir -p ~/.ssh
-#     ssh-keygen -t ed25519 -C "jeremyspofford@gmail.com" -f ~/.ssh/personal_id_ed25519 -N ""
-#     cat ~/.ssh/personal_id_ed25519.pub | xclip -selection clipboard
-#     eval "$(ssh-agent -s)"
-#     ssh-add ~/.ssh/personal_id_ed25519
-
-#     # Pause until user presses enter
-#     echo "SSH key generated and copied to clipboard"
-#     echo "Please add the key to GitHub and press enter to continue"
-#     read -p "Press enter to continue"
-# }
-
-
-# # Identify the operating system
-# OS=$(uname -s)
-
-# echo "Detected operating system: $OS"
-
-# if [ "$OS" == "Linux" ]; then
-#     echo "Installing packages for Linux"
-
-#     sudo add-apt-repository -y ppa:ansible/ansible
-#     sudo apt update -y && sudo apt upgrade -y
-#     sudo apt install -y software-properties-common \
-#         xclip \
-#         ansible
-#     sudo apt autoremove -y
-
-#     if [[ ! -f ~/.ssh/personal_id_ed25519.pub ]]; then
-#         generate_ssh_key
-#     fi
-
-#     if [[ ! -d $repo_name ]]; then
-#         git clone git@github.com:jeremyspofford/$repo_name.git $HOME/$repo_name
-#     fi
-
-#     ansible-playbook -i $HOME/$repo_name/inventory.ini $HOME/$repo_name/debian.yaml -K
-
-# # elif [ "$OS" == "Darwin" ]; then
-# #     brew install mise
-
-# #     generate_ssh_key
-
-# #     git clone git@github.com:jeremyspofford/$repo_name.git
-# #     cd $repo_name
-
-# #     ansible-playbook -i inventory.ini bootstrap.yaml
-
-# else
-#     echo "Unsupported operating system: $OS"
-#     exit 1
-# fi
-
-
-
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -110,17 +45,32 @@ run_ansible_and_continue() {
 #   ./bootstrap.sh
 }
 
+# # Generate SSH key
+generate_ssh_key() {
+    mkdir -p ~/.ssh
+    ssh-keygen -t ed25519 -C "23528024+jeremyspofford@users.noreply.github.com" -f ~/.ssh/personal_id_ed25519 -N ""
+    cat ~/.ssh/personal_id_ed25519.pub | xclip -selection clipboard
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/personal_id_ed25519
+
+    # Pause until user presses enter
+    log "SSH key generated and copied to clipboard"
+    log "Please add the key to GitHub and press enter to continue"
+    read -p "Press enter to continue"
+}
+
 clone_dotfiles() {
   log "Cloning dotfiles..."
   mkdir -p ~/dotfiles
   cd ~/dotfiles
-  yadm clone https://github.com/jeremyspofford/dotfiles.git  -f
+  yadm clone https://github.com/jeremyspofford/dotfiles.git
 #   yadm clone git@github.com:jeremyspofford/dotfiles.git
 }
 
 main() {
   detect_os_and_install_ansible
   run_ansible_and_continue
+  generate_ssh_key
   clone_dotfiles
   log "✅ Full bootstrap complete!"
 }
