ec029a9f3f6795e66fbfa9033e6d17522f2d34cc
Jeremy Spofford <23528024+jeremyspofford@users.noreply.github.com>
2025-04-28 19:50:23 -0400

    initialize stow

--- setup/init.sh
+++ setup/init.sh
@@ -23,18 +23,18 @@ install_ansible_ubuntu() {
 
 detect_os_and_install_ansible() {
   case "$(uname -s)" in
-    Darwin)
-      log "Detected macOS"
-      install_ansible_macos
-      ;;
-    Linux)
-      log "Detected Linux"
-      install_ansible_ubuntu
-      ;;
-    *)
-      echo "Unsupported OS: $(uname -s)"
-      exit 1
-      ;;
+  Darwin)
+    log "Detected macOS"
+    install_ansible_macos
+    ;;
+  Linux)
+    log "Detected Linux"
+    install_ansible_ubuntu
+    ;;
+  *)
+    echo "Unsupported OS: $(uname -s)"
+    exit 1
+    ;;
   esac
 }
 
@@ -43,31 +43,31 @@ run_ansible_and_continue() {
   ansible-playbook ~/dotfiles/setup/ansible/playbook.yml
 
   log "Running remaining shell bootstrap (GPG, yadm, 1Password)..."
-#   ./bootstrap.sh
+  #   ./bootstrap.sh
 }
 
 # Generate SSH key
 generate_ssh_key() {
-    mkdir -p ~/.ssh
-    echo -n "GitHub email: "
-    read email
-    ssh-keygen -t ed25519 -C $email -f ~/.ssh/personal_id_ed25519 -N ""
-    cat ~/.ssh/personal_id_ed25519.pub | xclip -selection clipboard
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/personal_id_ed25519
+  mkdir -p ~/.ssh
+  echo -n "GitHub email: "
+  read email
+  ssh-keygen -t ed25519 -C $email -f ~/.ssh/personal_id_ed25519 -N ""
+  cat ~/.ssh/personal_id_ed25519.pub | xclip -selection clipboard
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/personal_id_ed25519
 
-    # Pause until user presses enter
-    log "SSH key generated and copied to clipboard"
-    log "Please add the key to GitHub and press enter to continue"
-    read -p "Press enter to continue"
+  # Pause until user presses enter
+  log "SSH key generated and copied to clipboard"
+  log "Please add the key to GitHub and press enter to continue"
+  read -p "Press enter to continue"
 }
 
 # Install mise and tools
 mise_install() {
-    curl https://mise.run | sh
-    echo 'eval "$(/home/jeremy/.local/bin/mise activate bash)" >> ~/.bashrc'
-    # $HOME/.local/bin/mise install -y
-    mise install -y
+  curl https://mise.run | sh
+  echo 'eval "$(/home/jeremy/.local/bin/mise activate bash)" >> ~/.bashrc'
+  # $HOME/.local/bin/mise install -y
+  mise install -y
 }
 
 clone_dotfiles() {
@@ -75,17 +75,64 @@ clone_dotfiles() {
   mkdir -p ~/dotfiles
   cd ~/dotfiles
   yadm clone https://github.com/jeremyspofford/dotfiles.git
-#   yadm clone git@github.com:jeremyspofford/dotfiles.git
+  #   yadm clone git@github.com:jeremyspofford/dotfiles.git
+}
+
+backup_dotfiles() {
+  log "Backing up $HOME dotfiles"
+  cd $HOME
+
+  # Create a timestamp for the unique backup forlder
+  timestamp=$(date +"%Y%m%d_%H%M%S")
+  backup_folder="backup_$timestamp"
+
+  mkdir -p "$backup_folder"
+
+  # Array of files to be backed up
+  files_to_backup=(
+    .bash_aliases
+    .bash_profile
+    .bashrc
+    .gitconfig
+    .profile
+    .zshrc
+    .zshenv
+    .zsh_aliases
+    .zprofile
+    .vimrc
+  )
+
+  # Loop through the array and move each file to the backup folder
+  for file in "${files_to_backup[@]}"; do
+    if [ -f "$file" ]; then
+      log "Backing up $file"
+      mv "$file" "$backup_folder/$file"
+    else
+      log "File $file not found, skipping"
+    fi
+  done
+
+  log "Dotfiles backup complete"
+}
+
+stow_it() {
+  log "Syncing dotfiles with GNU stow"
+  cd $HOME/dotfiles/
+  stow .
+  log "Dotfiles sync complete"
 }
 
 main() {
+  if [[ ! ~/.ssh/personal_id_ed25519 ]]; then
+    generate_ssh_key
+  fi
   detect_os_and_install_ansible
   run_ansible_and_continue
   mise_install
-  # if [[ ! ~/.ssh/personal_id_ed25519 ]]; then
-    generate_ssh_key
-  # fi
-#  clone_dotfiles
+  backup_dotfiles
+  stow_it
+  #  clone_dotfiles
+
   log "✅ Full bootstrap complete!"
 }
 
