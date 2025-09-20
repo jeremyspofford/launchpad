940783aa55d5ffbf47f5222ef490dc3998c035bf
Jeremy Spofford <23528024+jeremyspofford@users.noreply.github.com>
2025-09-20 17:38:04 -0400

    Added sso logic to log into aws profile, improve secrets access via 1… (#15)
    * Added sso logic to log into aws profile, improve secrets access via 1password, and improve terminal startup time
    
    * removed trailing space from string value
    
    * feat: Add terminal toggle keybinding to Neovim
    
    Add Ctrl+` keybinding to toggle a right-side terminal in Neovim. Terminal persists between toggles and can be closed from within terminal mode.
    
    * refactor to use a commonrc file that is compatible between multiple os's

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -18,3 +18,48 @@ keymap.set("n", "<C-shift-p>", "<cmd>Telescope commands<cr>", { desc = "Command 
 -- keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
 -- keymap.set("n", "<C-Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 -- keymap.set("n", "<C-S-Tab>", "<cmd>bprev<cr>", { desc = "Previous buffer" })
+
+-- Terminal toggle (Ctrl + backtick)
+keymap.set("n", "<C-`>", function()
+  local term_buf = nil
+  local term_win = nil
+  
+  -- Find existing terminal buffer
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buftype == "terminal" then
+      term_buf = buf
+      -- Check if terminal is visible in any window
+      for _, win in ipairs(vim.api.nvim_list_wins()) do
+        if vim.api.nvim_win_get_buf(win) == buf then
+          term_win = win
+          break
+        end
+      end
+      break
+    end
+  end
+  
+  if term_win then
+    -- Terminal is visible, close it
+    vim.api.nvim_win_close(term_win, false)
+  else
+    -- Terminal not visible, open/create it
+    vim.cmd("rightbelow vsplit")
+    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+      -- Use existing terminal buffer
+      vim.api.nvim_win_set_buf(0, term_buf)
+    else
+      -- Create new terminal
+      vim.cmd("terminal")
+    end
+    -- Resize to reasonable width (40% of screen)
+    local width = math.floor(vim.o.columns * 0.4)
+    vim.api.nvim_win_set_width(0, width)
+  end
+end, { desc = "Toggle terminal to the right" })
+
+-- Terminal toggle from within terminal (Ctrl + backtick)
+keymap.set("t", "<C-`>", function()
+  local term_win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_close(term_win, false)
+end, { desc = "Close terminal from within terminal" })
