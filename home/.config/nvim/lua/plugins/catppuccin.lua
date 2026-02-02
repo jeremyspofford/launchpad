9e7dd7dc901a515dce2637d7ffa1bd4cf2af6506
Jeremy Spofford <23528024+jeremyspofford@users.noreply.github.com>
2026-02-02 10:23:27 -0500

    fix neovim configuration issues - disable ai plugin

--- home/.config/nvim/lua/plugins/catppuccin.lua
+++ home/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,56 +3,39 @@
 -- ============================================================================ --
 
 return {
-  -- Catppuccin theme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      transparent_background = false,
-      term_colors = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-        },
-      },
-    },
-  },
+	-- Catppuccin theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			flavour = "mocha",
+			background = {
+				-- light = "latte",
+				dark = "mocha",
+			},
+			transparent_background = false,
+			term_colors = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+				},
+			},
+		},
+	},
 
-  -- Auto dark mode (follows system theme)
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd.colorscheme("catppuccin-mocha")
-      end,
-      set_light_mode = function()
-        vim.api.nvim_set_option_value("background", "light", {})
-        vim.cmd.colorscheme("catppuccin-latte")
-      end,
-      update_interval = 1000,
-      fallback = "dark",
-    },
-  },
-
-  -- Set catppuccin as the default colorscheme for LazyVim
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
+	-- Set catppuccin as the default colorscheme for LazyVim
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha",
+		},
+	},
 }
